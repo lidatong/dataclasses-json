5a5c2bb2812daaa71de7a9e3d8495b7634ee0535
Charles Li <charles.dt.li@gmail.com>
2018-08-12 11:49:13 -0400

    Release v0.0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dataclasses-json",
-    version="0.0.8",
+    version="0.0.9",
     packages=find_packages(exclude=("tests*",)),
     author="lidatong",
     author_email="charles.dt.li@gmail.com",

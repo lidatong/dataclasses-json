601b35c2ad07d7927c9473c6cbf500d1fec3e307
Charles Li <charles.dt.li@gmail.com>
2018-08-12 10:58:24 -0400

    Rename encode/decode parameterization in test

--- tests/test_invariants.py
+++ tests/test_invariants.py
@@ -10,19 +10,18 @@ from tests.test_entities import (DataClassWithDeque, DataClassWithFrozenSet,
                                  DataClassWithList, DataClassWithOptional,
                                  DataClassWithSet, DataClassWithTuple)
 
-conss_to_strategies = [(DataClassWithList, lists, list),
-                       (DataClassWithSet, sets, set),
-                       (DataClassWithTuple, tuples, tuple),
-                       (DataClassWithFrozenSet, frozensets,
-                        frozenset),
-                       (DataClassWithDeque, deques, deque),
-                       (DataClassWithOptional, optionals,
-                        lambda x: x)]
+dcconss_strategies_conss = [(DataClassWithList, lists, list),
+                            (DataClassWithSet, sets, set),
+                            (DataClassWithTuple, tuples, tuple),
+                            (DataClassWithFrozenSet, frozensets, frozenset),
+                            (DataClassWithDeque, deques, deque),
+                            (DataClassWithOptional, optionals, lambda x: x)]
 example_input = [1]
 
 
-@given(one_of(*[strategy_fn(integers()).map(cons)
-                for cons, strategy_fn, _ in conss_to_strategies]))
-@examples(*[cons(f(example_input)) for cons, _, f in conss_to_strategies])
+@given(one_of(*[strategy_fn(integers()).map(dccons)
+                for dccons, strategy_fn, _ in dcconss_strategies_conss]))
+@examples(*[dccons(cons(example_input))
+            for dccons, _, cons in dcconss_strategies_conss])
 def test_generic_encode_and_decode_are_inverses(dc):
     assert dc.from_json(dc.to_json()) == dc

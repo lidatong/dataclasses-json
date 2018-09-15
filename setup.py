3ed644794742c2870f9f20fb2256c1dd42ac1899
Charles Li <charles.dt.li@gmail.com>
2018-09-15 11:52:28 -0400

    Release v0.0.16

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf8') as f:
 
 setup(
     name="dataclasses-json",
-    version="0.0.15",
+    version="0.0.16",
     packages=find_packages(exclude=("tests*",)),
     author="lidatong",
     author_email="charles.dt.li@gmail.com",

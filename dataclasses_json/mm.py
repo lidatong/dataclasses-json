9aaf94ad5edc7599054403fb6a83df714c3b938e
Sergei Solodovnikov <ssolodovnikov@quantenna.com>
2019-05-23 14:05:21 +0300

    Enhance enums support (#51) (#63) (#83)

--- dataclasses_json/mm.py
+++ dataclasses_json/mm.py
@@ -4,13 +4,14 @@ import warnings
 from dataclasses import MISSING, is_dataclass, fields as dc_fields
 from datetime import datetime
 from uuid import UUID
+from enum import Enum
 
 from marshmallow import fields, Schema, post_load
+from marshmallow_enum import EnumField
 
 from dataclasses_json.core import _is_supported_generic, _decode_dataclass
 from dataclasses_json.utils import (_is_collection, _is_optional,
                                     _issubclass_safe, _timestamp_to_dt_aware)
-
 
 
 class _TimestampField(fields.Field):
@@ -46,6 +47,7 @@ TYPES = {
     UUID: fields.UUID
 }
 
+
 def build_type(type_, options, mixin, field, cls):
     def inner(type_, options):
         if is_dataclass(type_):
@@ -67,6 +69,10 @@ def build_type(type_, options, mixin, field, cls):
 
         if origin in TYPES:
             return TYPES[origin](*args, **options)
+
+        if _issubclass_safe(origin, Enum):
+            return EnumField(enum=origin, by_value=True, *args, **options)
+
         warnings.warn(f"Unknown type {type_} at {cls.__name__}.{field.name}: {field.type} "
                       f"It's advised to pass the correct marshmallow type to `mm_field`.")
         return fields.Field(**options)

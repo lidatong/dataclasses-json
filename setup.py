978ce989668a41a38ca1f307ade92af27c6edc6b
Charles Li <charles.dt.li@gmail.com>
2019-06-16 11:25:51 -0400

    fix(marshmallow): pin to rc6 due to breaking .schema() with rc7

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     keywords="dataclasses json",
     install_requires=[
         "dataclasses;python_version=='3.6'",
-        "marshmallow>=3.0.0rc6",
+        "marshmallow==3.0.0rc6",
         "marshmallow-enum>=1.4.1",
         "typing-inspect>=0.4.0"
     ],

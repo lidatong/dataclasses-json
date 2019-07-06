6c4cc2e5276fedb8bb2346bfef3f2c24282345d8
Charles Li <charles.dt.li@gmail.com>
2019-07-06 19:00:47 -0400

    style: update setup.py to use single quotes

--- setup.py
+++ setup.py
@@ -4,26 +4,26 @@ with open('README.md', 'r', encoding='utf8') as f:
     readme = f.read()
 
 setup(
-    name="dataclasses-json",
-    version="0.2.8",
-    packages=find_packages(exclude=("tests*",)),
-    author="lidatong",
-    author_email="charles.dt.li@gmail.com",
-    description="Easily serialize dataclasses to and from JSON",
+    name='dataclasses-json',
+    version='0.2.8',
+    packages=find_packages(exclude=('tests*',)),
+    author='lidatong',
+    author_email='charles.dt.li@gmail.com',
+    description='Easily serialize dataclasses to and from JSON',
     long_description=readme,
     long_description_content_type='text/markdown',
-    url="https://github.com/lidatong/dataclasses-json",
-    license="Unlicense",
-    keywords="dataclasses json",
+    url='https://github.com/lidatong/dataclasses-json',
+    license='Unlicense',
+    keywords='dataclasses json',
     install_requires=[
-        "dataclasses;python_version=='3.6'",
-        "marshmallow==3.0.0rc6",
-        "marshmallow-enum>=1.4.1",
-        "typing-inspect>=0.4.0"
+        'dataclasses;python_version=="3.6"',
+        'marshmallow==3.0.0rc6',
+        'marshmallow-enum>=1.4.1',
+        'typing-inspect>=0.4.0'
     ],
-    python_requires=">=3.6",
+    python_requires='>=3.6',
     extras_require={
-        "dev": ["pytest", "ipython", "mypy>=0.710", "hypothesis"]
+        'dev': ['pytest', 'ipython', 'mypy>=0.710', 'hypothesis']
     },
     include_package_data=True
 )

b196c17bf20d73d54ec8036d32a6414270676078
Charles Li <charles.dt.li@gmail.com>
2019-11-03 08:23:37 -0500

    Deployed  with MkDocs version: 1.0.4

--- publish.sh
+++ publish.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-if [ -z $1 ]
-then
-  echo 'must supply tag'
-  exit 1
-else
-  . venv/bin/activate
-  git tag $1
-  portray on_github_pages
-  python setup.py sdist bdist_wheel
-  twine upload dist/*
-fi
+. venv/bin/activate
+git add .
+git commit -m "Release $1"
+git push
+git tag "$1"
+git push --tags
+hub release create "$1"
+portray on_github_pages
+python setup.py sdist bdist_wheel
+twine upload dist/*
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf8') as f:
 
 setup(
     name='dataclasses-json',
-    version='0.3.5',
+    version='0.3.6',
     packages=find_packages(exclude=('tests*',)),
     package_data={"dataclasses_json": ["py.typed"]},
     author='lidatong',
@@ -33,5 +33,9 @@ setup(
             'portray'
         ]
     },
-    include_package_data=True
+    include_package_data=True,
+    scripts=['publish.py']
 )
+
+
+

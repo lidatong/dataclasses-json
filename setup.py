3fb57326cb4378861f9022eaade8c579fdcd898c
Charles Li <charles.dt.li@gmail.com>
2019-12-19 07:33:01 -0500

    Release v0.3.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf8') as f:
 
 setup(
     name='dataclasses-json',
-    version='0.3.6',
+    version='0.3.7',
     packages=find_packages(exclude=('tests*',)),
     package_data={"dataclasses_json": ["py.typed"]},
     author='lidatong',
@@ -36,4 +36,5 @@ setup(
     include_package_data=True,
     scripts=['publish.py']
 )
+
 

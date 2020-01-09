bc4deb3fe7829ab9199d16246a73b710c77fcfb9
eqperes <eqperes@gmail.com>
2020-01-09 09:21:04 -0300

    add tests for good old python `dict`

--- tests/test_dict.py
+++ tests/test_dict.py
@@ -18,9 +18,16 @@ class DataWithTypedDict:
     metadata: Dict[str, Union[str, float]]
 
 
+@dataclass_json
+@dataclass(frozen=True)
+class DataWithPythonDict:
+    metadata: dict
+
+
 example_metadata_dict = {"some_data": "written_here", "some_score": 34.4}
 d_ex_1 = DataWithDict(metadata=example_metadata_dict)
 d_ex_2 = DataWithTypedDict(metadata=example_metadata_dict)
+d_ex_3 = DataWithPythonDict(metadata=example_metadata_dict)
 d_ex_as_dict = {"metadata": example_metadata_dict}
 
 
@@ -30,6 +37,9 @@ class TestEncoder:
 
     def test_dataclass_with_typed_dict(self):
         assert d_ex_2.to_dict() == d_ex_as_dict, f'Actual: {d_ex_2.to_dict()}, Expected: {d_ex_as_dict}'
+
+    def test_dataclass_with_python_dict(self):
+        assert d_ex_3.to_dict() == d_ex_as_dict, f'Actual: {d_ex_3.to_dict()}, Expected: {d_ex_as_dict}'
 
 
 class TestDecoder:
@@ -41,12 +51,17 @@ class TestDecoder:
         d_from_dict = DataWithTypedDict.from_dict(d_ex_as_dict)
         assert d_ex_2 == d_from_dict, f'Actual: {d_from_dict}, Expected: {d_ex_2}'
 
+    def test_dataclass_with_python_dict(self):
+        d_from_dict = DataWithPythonDict.from_dict(d_ex_as_dict)
+        assert d_ex_3 == d_from_dict, f'Actual: {d_from_dict}, Expected: {d_ex_3}'
+
 
 class TestValidator:
     @pytest.mark.parametrize('metadata_dict, is_valid', [
         ({}, True),
         (example_metadata_dict, True),
-        ({"is_boolean": True}, True)
+        ({"is_boolean": True}, True),
+        (None, False)
     ])
     def test_dataclass_with_dict(self, metadata_dict, is_valid):
         schema = DataWithDict.schema()
@@ -61,5 +76,16 @@ class TestValidator:
     ])
     def test_dataclass_with_typed_dict(self, metadata_dict, is_valid):
         schema = DataWithTypedDict.schema()
+        res = schema.validate({"metadata": metadata_dict})
+        assert not res == is_valid
+
+    @pytest.mark.parametrize('metadata_dict, is_valid', [
+        ({}, True),
+        (example_metadata_dict, True),
+        ({"is_boolean": True}, True),
+        (None, False)
+    ])
+    def test_dataclass_with_python_dict(self, metadata_dict, is_valid):
+        schema = DataWithPythonDict.schema()
         res = schema.validate({"metadata": metadata_dict})
         assert not res == is_valid

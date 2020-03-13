da09282197551ed36519ce8a864d730115aca6c3
Charles Li <charles.dt.li@gmail.com>
2020-03-13 07:30:56 -0400

    Revert "add caching, add tests (#175)"
    This reverts commit 2208d4ff720037b5195e3ece8125bb9dc4473d58.

--- setup.py
+++ setup.py
@@ -21,8 +21,7 @@ setup(
         'marshmallow>=3.3.0,<4.0.0',
         'marshmallow-enum>=1.5.1,<2.0.0',
         'typing-inspect>=0.4.0',
-        'stringcase==1.2.0,<2.0.0',
-        'cachetools==4.0.0,<5.0.0'
+        'stringcase==1.2.0,<2.0.0'
     ],
     python_requires='>=3.6',
     extras_require={

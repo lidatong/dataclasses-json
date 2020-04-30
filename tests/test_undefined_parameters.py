9949cb79e31c14f740a3936256714512cf660c6a
Veith Röthlingshöfer <veith@understand.ai>
2020-04-30 11:19:29 +0200

    Rename undefined parameter test cases to be more descriptive

--- tests/test_undefined_parameters.py
+++ tests/test_undefined_parameters.py
@@ -87,22 +87,21 @@ def boss_json():
 
 class TestCatchAllUndefinedParameters:
 
-    def test_undefined_parameters_catch_all_invalid_back(self,
-                                                         invalid_response):
+    def test_it_dumps_undefined_parameters_back(self, invalid_response):
         dump = UnknownAPIDump.from_dict(invalid_response)
         inverse_dict = dump.to_dict()
         assert inverse_dict == invalid_response
 
-    def test_undefined_parameters_catch_all_valid(self, valid_response):
+    def test_dump_has_no_undefined_parameters_if_not_given(self,
+                                                           valid_response):
         dump = UnknownAPIDump.from_dict(valid_response)
         assert dump.catch_all == {}
 
-    def test_undefined_parameters_catch_all_no_field(self, invalid_response):
+    def test_it_requires_a_catch_all_field(self, invalid_response):
         with pytest.raises(UndefinedParameterError):
             UnknownAPIDumpNoCatchAllField.from_dict(invalid_response)
 
-    def test_undefined_parameters_catch_all_multiple_fields(self,
-                                                            invalid_response):
+    def test_it_requires_exactly_one_catch_all_field(self, invalid_response):
         @dataclass_json(undefined=Undefined.INCLUDE)
         @dataclass()
         class UnknownAPIDumpMultipleCatchAll:
@@ -114,8 +113,7 @@ class TestCatchAllUndefinedParameters:
         with pytest.raises(UndefinedParameterError):
             UnknownAPIDumpMultipleCatchAll.from_dict(invalid_response)
 
-    def test_undefined_parameters_catch_all_works_with_letter_case(self,
-                                                                   invalid_response_camel_case):
+    def test_it_works_with_letter_case(self, invalid_response_camel_case):
         @dataclass_json(undefined=Undefined.INCLUDE,
                         letter_case=LetterCase.CAMEL)
         @dataclass()
@@ -128,24 +126,21 @@ class TestCatchAllUndefinedParameters:
         assert {"undefinedFieldName": [1, 2, 3]} == dump.catch_all
         assert invalid_response_camel_case == dump.to_dict()
 
-    def test_undefined_parameters_catch_all_raises_if_initialized_with_catch_all_field_name(
-            self,
-            valid_response):
+    def test_catch_all_field_name_cant_be_a_primitive_parameter(self,
+                                                                valid_response):
         valid_response["catch_all"] = "some-value"
         with pytest.raises(UndefinedParameterError):
             UnknownAPIDump.from_dict(valid_response)
 
-    def test_undefined_parameters_catch_all_initialized_with_dict_and_more_unknown(
-            self,
-            invalid_response):
+    def test_catch_all_field_can_be_initialized_with_dict(self,
+                                                          invalid_response):
         invalid_response["catch_all"] = {"someValue": "some-stuff"}
         dump = UnknownAPIDump.from_dict(invalid_response)
         assert dump.catch_all == {"someValue": "some-stuff",
                                   "undefined_field_name": [1, 2, 3]}
 
-    def test_undefined_parameters_raises_with_default_argument_and_supplied_catch_all_name(
-            self,
-            invalid_response):
+    def test_it_raises_with_default_argument_and_catch_all_field_name(self,
+                                                                      invalid_response):
         @dataclass_json(undefined="include")
         @dataclass()
         class UnknownAPIDumpDefault:
@@ -157,9 +152,8 @@ class TestCatchAllUndefinedParameters:
         with pytest.raises(UndefinedParameterError):
             UnknownAPIDumpDefault.from_dict(invalid_response)
 
-    def test_undefined_parameters_doesnt_raise_with_default(self,
-                                                            valid_response,
-                                                            invalid_response):
+    def test_catch_all_field_can_have_default(self, valid_response,
+                                              invalid_response):
         @dataclass_json(undefined="include")
         @dataclass()
         class UnknownAPIDumpDefault:
@@ -172,9 +166,8 @@ class TestCatchAllUndefinedParameters:
         assert from_valid.catch_all is None
         assert {"undefined_field_name": [1, 2, 3]} == from_invalid.catch_all
 
-    def test_undefined_parameters_doesnt_raise_with_default_factory(self,
-                                                                    valid_response,
-                                                                    invalid_response):
+    def test_catch_all_field_can_have_default_factory(self, valid_response,
+                                                      invalid_response):
         @dataclass_json(undefined="include")
         @dataclass()
         class UnknownAPIDumpDefault(DataClassJsonMixin):
@@ -187,7 +180,7 @@ class TestCatchAllUndefinedParameters:
         assert from_valid.catch_all == {}
         assert {"undefined_field_name": [1, 2, 3]} == from_invalid.catch_all
 
-    def test_undefined_parameters_catch_all_nested_schema(self, boss_json):
+    def test_it_works_with_nested_schemata(self, boss_json):
         @dataclass_json(undefined=Undefined.INCLUDE)
         @dataclass(frozen=True)
         class Minion:
@@ -205,7 +198,7 @@ class TestCatchAllUndefinedParameters:
         assert {"UNKNOWN_PROPERTY": "value"} == boss.minions[0].catch_all
         assert {} == boss.minions[1].catch_all
 
-    def test_undefined_parameters_catch_all_schema_dump(self, boss_json):
+    def test_it_dumps_nested_schemata_correctly(self, boss_json):
         import json
 
         @dataclass_json(undefined=Undefined.INCLUDE)
@@ -225,7 +218,7 @@ class TestCatchAllUndefinedParameters:
         assert "".join(boss_json.replace('\n', '').split()) == "".join(
             Boss.schema().dumps(boss).replace('\n', '').split())
 
-    def test_undefined_parameters_catch_all_schema_roundtrip(self, boss_json):
+    def test_it_preserves_nested_schemata_in_roundtrip(self, boss_json):
         @dataclass_json(undefined=Undefined.INCLUDE)
         @dataclass(frozen=True)
         class Minion:
@@ -254,16 +247,15 @@ class TestCatchAllUndefinedParameters:
         dump = UnknownAPIDumpFromString.from_dict(invalid_response)
         assert {"undefined_field_name": [1, 2, 3]} == dump.catch_all
 
-    def test_undefined_parameters_catch_all_init_valid(self, valid_response):
+    def test_it_works_with_valid_dict_expansion(self, valid_response):
         dump = UnknownAPIDump(**valid_response)
         assert dump.catch_all == {}
 
-    def test_undefined_parameters_catch_all_init_invalid(self,
-                                                         invalid_response):
+    def test_it_works_with_invalid_dict_expansion(self, invalid_response):
         dump = UnknownAPIDump(**invalid_response)
         assert {"undefined_field_name": [1, 2, 3]} == dump.catch_all
 
-    def test_undefined_parameters_catch_all_init_args(self):
+    def test_it_creates_dummy_keys_for_init_args(self):
         dump = UnknownAPIDump("some-endpoint", {"some-data": "foo"}, "unknown1",
                               "unknown2", undefined="123")
         assert dump.endpoint == "some-endpoint"
@@ -272,7 +264,7 @@ class TestCatchAllUndefinedParameters:
                                   '_UNKNOWN1': 'unknown2',
                                   "undefined": "123"}
 
-    def test_undefined_parameters_catch_all_init_args_kwargs_mixed(self):
+    def test_it_creates_dummy_keys_for_init_args_kwargs_mix(self):
         dump = UnknownAPIDump("some-endpoint", {"some-data": "foo"}, "unknown1",
                               "unknown2", catch_all={"bar": "example"},
                               undefined="123")
@@ -282,8 +274,9 @@ class TestCatchAllUndefinedParameters:
                                   '_UNKNOWN1': 'unknown2',
                                   "bar": "example", "undefined": "123"}
 
-    def test_undefined_parameters_catch_all_default_no_undefined(self,
-                                                                 valid_response):
+    def test_it_doesnt_dump_the_default_value_without_undefined_parameters(
+            self,
+            valid_response):
         @dataclass_json(undefined="include")
         @dataclass()
         class UnknownAPIDumpDefault:
@@ -294,9 +287,8 @@ class TestCatchAllUndefinedParameters:
         dump = UnknownAPIDumpDefault.from_dict(valid_response)
         assert dump.to_dict() == valid_response
 
-    def test_undefined_parameters_catch_all_default_factory_init_converts_factory(
-            self,
-            valid_response):
+    def test_it_dumps_default_factory_without_undefined_parameters(self,
+                                                                   valid_response):
         @dataclass_json(undefined="include")
         @dataclass()
         class UnknownAPIDumpDefault:
@@ -310,48 +302,34 @@ class TestCatchAllUndefinedParameters:
 
 class TestRaiseUndefinedParameters:
 
-    def test_undefined_parameters_raise_invalid(self, invalid_response):
+    def test_it_raises_with_undefined_parameters(self, invalid_response):
         with pytest.raises(UndefinedParameterError):
             WellKnownAPIDump.from_dict(invalid_response)
 
-    def test_undefined_parameters_raise_valid(self, valid_response):
+    def test_it_doesnt_raise_with_known_parameters(self, valid_response):
         assert valid_response == WellKnownAPIDump.from_dict(
             valid_response).to_dict()
 
-    def test_undefined_parameters_raise_nested_schema(self, boss_json):
-        @dataclass_json(undefined=Undefined.RAISE)
-        @dataclass(frozen=True)
-        class Minion:
-            name: str
-
-        @dataclass_json(undefined=Undefined.EXCLUDE)
-        @dataclass(frozen=True)
-        class Boss:
-            minions: List[Minion]
-
-        with pytest.raises(marshmallow.exceptions.ValidationError):
-            Boss.schema().loads(boss_json)
-
-    def test_undefined_parameters_raise_init(self, invalid_response):
+    def test_it_has_python_semantics_in_init(self, invalid_response):
         with pytest.raises(TypeError):
             WellKnownAPIDump(**invalid_response)
 
 
 class TestIgnoreUndefinedParameters:
 
-    def test_undefined_parameters_ignore(self, valid_response,
-                                         invalid_response):
+    def test_it_ignores_undefined_parameters(self, valid_response,
+                                             invalid_response):
         from_valid = DontCareAPIDump.from_dict(valid_response)
         from_invalid = DontCareAPIDump.from_dict(invalid_response)
         assert from_valid == from_invalid
 
-    def test_undefined_parameters_ignore_to_dict(self, invalid_response,
-                                                 valid_response):
+    def test_it_does_not_dump_undefined_parameters(self, invalid_response,
+                                                   valid_response):
         dump = DontCareAPIDump.from_dict(invalid_response)
         dump_dict = dump.to_dict()
         assert valid_response == dump_dict
 
-    def test_undefined_parameters_ignore_nested_schema(self, boss_json):
+    def test_it_ignores_nested_schemata(self, boss_json):
         @dataclass_json(undefined=Undefined.EXCLUDE)
         @dataclass(frozen=True)
         class Minion:
@@ -367,15 +345,15 @@ class TestIgnoreUndefinedParameters:
         assert boss.minions == [Minion(name="evil minion"),
                                 Minion(name="very evil minion")]
 
-    def test_undefined_parameters_ignore_init_args(self):
+    def test_it_ignores_undefined_init_args(self):
         dump = DontCareAPIDump("some-endpoint", {"some-data": "foo"},
                                "unknown1",
                                "unknown2", undefined="123")
         assert dump.endpoint == "some-endpoint"
         assert dump.data == {"some-data": "foo"}
 
-    def test_undefined_parameters_ignore_init_invalid(self, invalid_response,
-                                                      valid_response):
+    def test_it_ignores_undefined_init_kwargs(self, invalid_response,
+                                              valid_response):
         dump_invalid = DontCareAPIDump(**invalid_response)
         dump_valid = DontCareAPIDump(**valid_response)
         assert dump_valid == dump_invalid
@@ -383,8 +361,22 @@ class TestIgnoreUndefinedParameters:
 
 class TestMiscellaneousUndefinedParameters:
 
-    def test_undefined_parameters_catch_all_ignore_mix_nested_schema(self,
-                                                                     boss_json):
+    def test_it_raises_with_nested_schemata(self, boss_json):
+        @dataclass_json(undefined=Undefined.RAISE)
+        @dataclass(frozen=True)
+        class Minion:
+            name: str
+
+        @dataclass_json(undefined=Undefined.EXCLUDE)
+        @dataclass(frozen=True)
+        class Boss:
+            minions: List[Minion]
+
+        with pytest.raises(marshmallow.exceptions.ValidationError):
+            Boss.schema().loads(boss_json)
+
+    def test_it_works_with_catch_all_ignore_mix_nested_schemata(self,
+                                                                boss_json):
         @dataclass_json(undefined=Undefined.EXCLUDE)
         @dataclass(frozen=True)
         class Minion:
@@ -401,7 +393,7 @@ class TestMiscellaneousUndefinedParameters:
         assert Minion(name="very evil minion") == boss.minions[1]
         assert {"UNKNOWN_PROPERTY": "value"} == boss.catch_all
 
-    def test_string_only_accepts_valid_actions(self):
+    def test_it_only_acceps_valid_actions_as_string(self):
         with pytest.raises(UndefinedParameterError):
             @dataclass_json(undefined="not sure what this is supposed to do")
             @dataclass()

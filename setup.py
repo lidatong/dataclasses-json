993faed0a3ba80a3d82bf83aef0a52d016c55fc9
Charles Li <charles.dt.li@gmail.com>
2020-05-29 16:18:57 -0500

    Deployed  with MkDocs version: 1.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf8') as f:
 
 setup(
     name='dataclasses-json',
-    version='0.4.3',
+    version='0.4.4',
     packages=find_packages(exclude=('tests*',)),
     package_data={"dataclasses_json": ["py.typed"]},
     author='lidatong',
@@ -38,6 +38,8 @@ setup(
     include_package_data=True,
     scripts=['publish.py']
 )
+
+
 
 
 

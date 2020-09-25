6672276bb7fb2e868bfa46fab65e30149067c124
Mike Brancato <mike@mikebrancato.com>
2020-09-25 19:02:31 -0400

    Add encoder and decoder exception handling

--- dataclasses_json/core.py
+++ dataclasses_json/core.py
@@ -107,7 +107,14 @@ def _encode_overrides(kvs, overrides, encode_json=False):
             k = letter_case(k) if letter_case is not None else k
 
             encoder = overrides[original_key].encoder
-            v = encoder(v) if encoder is not None else v
+            # v = encoder(v) if encoder is not None else v
+            if encoder is not None:
+                try:
+                    v = encoder(v)
+                except:
+                    raise ValueError(
+                        f"Encoder encountered an error with field '{k}'"
+                    )
 
         if encode_json:
             v = _encode_json_type(v)
@@ -156,7 +163,13 @@ def _decode_dataclass(cls, kvs, infer_missing):
         if not field.init:
             continue
 
-        field_value = kvs[field.name]
+        try:
+            field_value = kvs[field.name]
+        except KeyError:
+            raise KeyError(
+                f"Required field '{field.name}' not found while "
+                f"decoding into dataclass '{cls.__name__}'"
+            )
         field_type = types[field.name]
         if field_value is None and not _is_optional(field_type):
             warning = (f"value of non-optional type {field.name} detected "
@@ -184,8 +197,15 @@ def _decode_dataclass(cls, kvs, infer_missing):
             if field_type is type(field_value):
                 init_kwargs[field.name] = field_value
             else:
-                init_kwargs[field.name] = overrides[field.name].decoder(
-                    field_value)
+                try:
+                    init_kwargs[field.name] = overrides[field.name].decoder(
+                        field_value
+                    )
+                except:
+                    raise ValueError(
+                        f"Decoder encountered an error with field '{field.name}'"
+                        f"in '{cls.__name__}'"
+                    )
         elif is_dataclass(field_type):
             # FIXME this is a band-aid to deal with the value already being
             # serialized when handling nested marshmallow schema

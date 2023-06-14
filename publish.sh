a0d51fa8b32214a816306c1b806e900c033e0128
Charles Li <charles.dt.li@gmail.com>
2023-06-14 09:17:17 -0500

    chore: annotated tag

--- publish.sh
+++ publish.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -e
+
 . venv/bin/activate
 git add .
 git commit -m "chore: release $1"
-git tag "$1"
+git tag -a "$1"
 git push --follow-tags
 gh release create "$1" --latest --generate-notes --verify-tag
 portray on_github_pages

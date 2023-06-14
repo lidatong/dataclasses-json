6a4c07cec67547e860aa787952efa3edc6101e8d
Charles Li <charles.dt.li@gmail.com>
2023-06-14 09:14:59 -0500

    chore: atomic commit + tag push and add useful flags to gh release

--- publish.sh
+++ publish.sh
@@ -3,10 +3,9 @@
 . venv/bin/activate
 git add .
 git commit -m "chore: release $1"
-git push
 git tag "$1"
-git push --tags
-gh release create "$1"
+git push --follow-tags
+gh release create "$1" --latest --generate-notes --verify-tag
 portray on_github_pages
 # python setup.py sdist bdist_wheel
 # twine upload dist/*

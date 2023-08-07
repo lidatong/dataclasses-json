57a9f0addb7e10bc452cd5aba9a0044f8b27dc23
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2023-08-08 00:51:47 +0100

    Added more tests to collection of unions

--- tests/test_collection_of_unions.py
+++ tests/test_collection_of_unions.py
@@ -19,45 +19,154 @@ class TestOtherChild:
 
 @dataclass_json
 @dataclass(frozen=True)
-class NotWorkingDictUnion:
+class DictUnion:
     d: Dict[str, Union[TestChild, TestOtherChild]]
 
 
 @dataclass_json
 @dataclass(frozen=True)
-class NotWorkingListUnion:
+class ListUnion:
     l: List[Union[TestChild, TestOtherChild]]
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class Player:
+    name: str
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class Team:
+    roster: list[int | Player]
+    roster_backup: dict[int, int | Player]
 
 
 class TestCollectionOfUnions:
     def test_dict(self):
         data = {
-            "d": {
-                "child" : {
-                    "some_field" : 1
+            'd': {
+                'child' : {
+                    'some_field' : 1
                 },
-                "other_child" : {
-                    "other_field" : 2
+                'other_child' : {
+                    'other_field' : 2
                 }
             }
         }
         json_str = json.dumps(data)
-        obj = NotWorkingDictUnion.from_json(json_str)
+        obj = DictUnion.from_json(json_str)
 
         assert type(obj.d['child']) in (TestChild, TestOtherChild)
 
     def test_list(self):
         data = {
-            "l": [
+            'l': [
                 {
-                    "some_field" : 1
+                    'some_field' : 1
                 },
                 {
-                    "other_field" : 1
+                    'other_field' : 1
                 }
             ]
         }
         json_str = json.dumps(data)
-        obj = NotWorkingListUnion.from_json(json_str)
+        obj = ListUnion.from_json(json_str)
 
         assert type(obj.l[0]) in (TestChild, TestOtherChild)
+
+    def test_int(self):
+        data = {
+            'roster': [
+                1,
+                2,
+                3
+            ],
+            'roster_backup': {
+                1: 5,
+                2: 3,
+                3: 2
+            }
+        }
+        json_str = json.dumps(data)
+        obj: Team = ListUnion.from_json(json_str)
+
+        assert type(obj.roster[0]) is int and type(obj.roster_backup[1]) is int
+
+    def test_dataclass(self):
+        data = {
+            'roster': [
+                {
+                    'player1'
+                },
+                {
+                    'player2'
+                },
+                {
+                    'player3'
+                }
+            ],
+            'roster_backup': {
+                1: {
+                    'player1'
+                },
+                2: {
+                    'player2'
+                },
+                3: {
+                    'player3'
+                }
+            }
+        }
+        json_str = json.dumps(data)
+        obj: Team = ListUnion.from_json(json_str)
+
+        assert type(obj.roster[0]) is Player and type(obj.roster_backup[1]) is Player
+
+    def test_mixed(self):
+        data = {
+            'roster': [
+                {
+                    'player1'
+                },
+                {
+                    'player2'
+                },
+                {
+                    'player3'
+                }
+            ],
+            'roster_backup': {
+                1: 5,
+                2: 3,
+                3: 2
+            }
+        }
+        json_str = json.dumps(data)
+        obj: Team = ListUnion.from_json(json_str)
+
+        assert type(obj.roster[0]) is Player and type(obj.roster_backup[1]) is int
+
+    def test_mixed_inverse(self):
+        data = {
+            'roster': [
+                1,
+                2,
+                3
+            ],
+            'roster_backup': {
+                1: {
+                    'player1'
+                },
+                2: {
+                    'player2'
+                },
+                3: {
+                    'player3'
+                }
+            }
+        }
+        json_str = json.dumps(data)
+        obj: Team = ListUnion.from_json(json_str)
+
+        assert type(obj.roster[0]) is int and type(obj.roster_backup[1]) is Player

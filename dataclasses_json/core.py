e21fb9f55ae9d71c653b34bdca7a59eae23737f3
George Zubrienko <gzu@ecco.com>
2023-11-09 11:01:52 +0100

    Review comments

--- dataclasses_json/core.py
+++ dataclasses_json/core.py
@@ -391,13 +391,12 @@ def _decode_items(type_args, xs, infer_missing):
 
     if _isinstance_safe(type_args, Collection) and not _issubclass_safe(type_args, Enum):
         if len(type_args) == len(xs):
-            return list(_decode_item(type_arg, xs[type_ix]) for type_ix, type_arg in enumerate(type_args))
-        if len(type_args) == 1:
-            return list(_decode_item(type_arg, x) for type_arg, x in product(type_args, xs))
+            return list(_decode_item(type_arg, x) for type_arg, x in zip(type_args, xs))
         else:
-            raise TypeError(f"Number of types specified in collection type {str(type_args)} "
-                            f"is greater than one and does not match number of elements in the collection. "
-                            f"Please review the type hint for this field.")
+            raise TypeError(f"Number of types specified in the collection type {str(type_args)} "
+                            f"does not match number of elements in the collection. In case you are working with tuples,"
+                            f"take a look at this document "
+                            f"https://docs.python.org/3/library/typing.html#annotating-tuples.")
     return list(_decode_item(type_args, x) for x in xs)
 
 

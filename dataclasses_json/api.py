08266a6201b7a0803afb9365f548cdf01d53ef20
Kerling Philipp (MTN MTC) <Philipp.Kerling@moteon.com>
2025-04-29 09:39:40 +0200

    Fix typing of LetterCase
    Multiple issues prevented the letter_case argument on the dataclass_json
    decorator to work correctly with strict type checking settings and
    PyRight:
    * While config() etc. correctly accept also a callable instead of only
      an enum member, the dataclass_json decorator did not do this. Since
      the enum members are mapped to callables, which cannot be used as base
      types, they cannot be used as argument values of type LetterCase.
    * All functions in stringcase.py were missing type hints.
    * The LetterCase enum members need concrete type definitions, so that
      there arguments are sufficiently constrained and they are compatible
      with the correct callable type `Callable[[str], str]`. PyRight would
      otherwise infer `Callable[..., str]`, which is not narrow enough.
    
    These changes allow a JSON dataclass with letter_case argument to pass
    type testing with basedpyright. The code is fully backwards-compatible,
    as only type information is enhanced.

--- dataclasses_json/api.py
+++ dataclasses_json/api.py
@@ -2,7 +2,7 @@ import abc
 import json
 from typing import Any, Callable, Dict, List, Optional, Tuple, Type, TypeVar, Union, overload
 
-from dataclasses_json.cfg import config, LetterCase
+from dataclasses_json.cfg import config, LetterCase, LetterCaseCallable
 from dataclasses_json.core import (Json, _ExtendedEncoder, _asdict,
                                    _decode_dataclass)
 from dataclasses_json.mm import (JsonData, SchemaType, build_schema)
@@ -103,16 +103,16 @@ class DataClassJsonMixin(abc.ABC):
 
 
 @overload
-def dataclass_json(_cls: None = ..., *, letter_case: Optional[LetterCase] = ...,
+def dataclass_json(_cls: None = ..., *, letter_case: Union[LetterCaseCallable, LetterCase, None] = ...,
                    undefined: Optional[Union[str, Undefined]] = ...) -> Callable[[Type[T]], Type[T]]: ...
 
 
 @overload
-def dataclass_json(_cls: Type[T], *, letter_case: Optional[LetterCase] = ...,
+def dataclass_json(_cls: Type[T], *, letter_case: Union[LetterCaseCallable, LetterCase, None] = ...,
                    undefined: Optional[Union[str, Undefined]] = ...) -> Type[T]: ...
 
 
-def dataclass_json(_cls: Optional[Type[T]] = None, *, letter_case: Optional[LetterCase] = None,
+def dataclass_json(_cls: Optional[Type[T]] = None, *, letter_case: Union[LetterCaseCallable, LetterCase, None] = None,
                    undefined: Optional[Union[str, Undefined]] = None) -> Union[Callable[[Type[T]], Type[T]], Type[T]]:
     """
     Based on the code in the `dataclasses` module to handle optional-parens
@@ -132,7 +132,7 @@ def dataclass_json(_cls: Optional[Type[T]] = None, *, letter_case: Optional[Lett
     return wrap(_cls)
 
 
-def _process_class(cls: Type[T], letter_case: Optional[LetterCase],
+def _process_class(cls: Type[T], letter_case: Union[LetterCaseCallable, LetterCase, None],
                    undefined: Optional[Union[str, Undefined]]) -> Type[T]:
     if letter_case is not None or undefined is not None:
         cls.dataclass_json_config = config(letter_case=letter_case,  # type: ignore[attr-defined]
